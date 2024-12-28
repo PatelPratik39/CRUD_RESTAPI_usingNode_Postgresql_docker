6cb4ce9f08b4519aa5fa2692b02593b39768ab2c
PatelPratik39 <ptl.pratik717@gmail.com>
2024-12-28 16:15:27 -0600

    docker image generated for postgres database connection

--- index.js
+++ index.js
@@ -1,30 +1,24 @@
-import express from 'express'
-import cors from 'cors'
-import dotenv from 'dotenv'
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 import chalk from "chalk";
-
-
 
 dotenv.config();
 
-const app = express()
-const port = process.env.PORT || 8000
-
+const app = express();
+const port = process.env.PORT || 8000;
 
 // -------------Middleware block------------
 app.use(express.json());
 app.use(cors);
 
-
-
 // -------- ROUTES ---------------
-
 
 // ------------ Error handling ----------------
 
-
 // -------- Server Running -----------
 app.listen(port, () => {
-    console.log(chalk.bold.green(`Server is running on http://localhost:${port}`));
-    
-})
+  console.log(
+    chalk.bold.green(`Server is running on http://localhost:${port}`)
+  );
+});
