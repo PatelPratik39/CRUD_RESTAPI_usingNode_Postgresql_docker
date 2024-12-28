38714019decae3d78ff4ed3eb1eb94e26e75d63b
PatelPratik39 <ptl.pratik717@gmail.com>
2024-12-28 16:48:51 -0600

    USerModel is ready with queries

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,11 +1,37 @@
 import pool from "../config/db.js";
 
-export const getAllUserService = async () => {}; // get all users
+export const getAllUserService = async () => {
+  // get all users
+  const result = pool.query("SELECT * FROM users");
+  return result.rows;
+};
 
-export const getUserByIdService = async () => {}; // get a user by id
+export const getUserByIdService = async (id) => {
+  // get a user by id
+  const result = await pool.query("SELECT * FROM users where id = $1", [id]);
+  return result.rows[0];
+};
 
-export const createUserService = async () => {}; // create a user
+export const createUserService = async (name, email) => {
+  const result = await pool.query(
+    "INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *",
+    [name, email]
+  );
+  return result.rows[0];
+}; // create a user
 
-export const updateUserService = async () => {}; // udpate a user by id
+export const updateUserService = async (id, name, email) => {
+  const result = await pool.query(
+    "UPDATE users SET name=$1, email=$2, WHERE id=$3 RETURNING *",
+    [name, email, id]
+  );
+  return result.rows[0];
+}; // udpate a user by id
 
-export const deleteUserService = async () => {}; // delete User by id
+export const deleteUserService = async (id) => {
+  const result = await pool.query(
+    "DELETE FROM users WHERE id = $1 RETURNING *",
+    [id]
+  );
+  return result.rows[0];
+}; // delete User by id
