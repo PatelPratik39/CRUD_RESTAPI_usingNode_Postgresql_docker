1033524c7731d35efbb0f243eb6cee1df6c016a4
PatelPratik39 <ptl.pratik717@gmail.com>
2024-12-28 16:01:15 -0600

    Basic Server is running with customized log colors

---
+++ index.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import chalk from "chalk";
+
+
+
+dotenv.config();
+
+const app = express()
+const port = process.env.PORT || 8000
+
+
+// -------------Middleware block------------
+app.use(express.json());
+app.use(cors);
+
+
+
+// -------- ROUTES ---------------
+
+
+// ------------ Error handling ----------------
+
+
+// -------- Server Running -----------
+app.listen(port, () => {
+    console.log(chalk.bold.green(`Server is running on http://localhost:${port}`));
+    
+})
